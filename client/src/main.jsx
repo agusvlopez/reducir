a9a0ab58aa761375403c854139dfa298b7dcf4e7
agusvlopez <agusvlopez@gmail.com>
2025-06-05 00:42:41 -0300

    Feat: add NextUI and fix Tailwind CSS configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { StrictMode } from 'react';
+import { HeroUIProvider } from '@heroui/react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <HeroUIProvider>
+        <App />
+      </HeroUIProvider>
+    </BrowserRouter>
+  </StrictMode>,
 )
